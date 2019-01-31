2ff60e991cc9ea924d5b8fc979da682a70e6c147
Lucas Lima <lucas.lima@mblabs.com.br>
2019-01-31 20:46:13 -0200

    Twofer

---
+++ hello-world/src/main/kotlin/TwoFer.kt
@@ -0,0 +1,7 @@
+fun twofer(): String {
+    return "One for you, one for me."
+}
+
+fun twofer(text: String): String {
+    return "One for $text, one for me."
+}
--- hello-world/src/test/kotlin/HelloWorldTest.kt
+++ hello-world/src/test/kotlin/HelloWorldTest.kt
@@ -1,7 +1,7 @@
 import org.junit.Test
 import kotlin.test.assertEquals
 
-class HelloWorldTest {
+class TwoFer {
 
     @Test
     fun helloWorldTest() {
